82b24f119047b61d72fb883fe404d35b7098fb04
Dante Montford <85903928+DanteArcanaelMontford@users.noreply.github.com>
2021-10-13 23:54:35 -0300

    Fisrt-commit

---
+++ qrcore-generator/src/App.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+import QRCodeCanvas from './QRCodeCanvas';
+
+function App() {
+
+  const [text, setText] = useState("")
+
+  return (
+    <div className="App">
+      <input onChange={(element)=>(setText(element.target.value))} value={text}></input>
+      <QRCodeCanvas text={text}></QRCodeCanvas>
+    </div>
+  );
+}
+
+export default App;
